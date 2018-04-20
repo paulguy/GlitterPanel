90b9fe5e83d9d8465416b50b432b579147bbdeb3
coon <coon@mailbox.org>
2018-04-20 19:49:15 +0200

    build fix

--- GP.h
+++ GP.h
@@ -25,11 +25,11 @@ struct GPpacket *makePacket();
 void clearPanel(struct usb_dev_handle *dev);
 void closePanel(struct usb_dev_handle *dev);
 void swap16bytes(unsigned short int *array);
-inline void pset(unsigned short int *array, unsigned int x, unsigned int y);
-inline void pclear(unsigned short int *array, unsigned int x, unsigned int y);
-inline void ptoggle(unsigned short int *array, unsigned int x, unsigned int y);
-inline int pget(unsigned short int *array, unsigned int x, unsigned int y);
-inline void clearPacket(struct GPpacket *packet);
+// inline void pset(unsigned short int *array, unsigned int x, unsigned int y);
+// inline void pclear(unsigned short int *array, unsigned int x, unsigned int y);
+// inline void ptoggle(unsigned short int *array, unsigned int x, unsigned int y);
+// inline int pget(unsigned short int *array, unsigned int x, unsigned int y);
+void clearPacket(struct GPpacket *packet);
 void drawLine(int x1, int y1, int x2, int y2, unsigned short int *array);
 void drawRay(int x, int y, int length, double angle, unsigned short int *array);
 
